2122c8031d08369cea36b55d6907098b89a6a90c
h28a3 <at.h.s_24328@iCloud.com>
2024-07-21 08:37:12 +0900

    Add files via upload

---
+++ code/header.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#define N 4	//ヒットアンドブローゲームの桁数(1~10)
+
+int answer[10], input[N];
+
+void Initialization(void);
+
+void get_answer(void);
+
+void get_player_answer(int count);
+
