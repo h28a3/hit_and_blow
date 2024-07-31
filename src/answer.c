2671f4ea483d3ac7283ae08e9d8fafd2f133837d
h28a3 <at.h.s_24328@iCloud.com>
2024-07-31 21:25:40 +0900

    Update answer.c

--- src/answer.c
+++ src/answer.c
@@ -1,15 +1,15 @@
 #include"header.h"
 
 void make_answer(void) {
-    int i, temp, rand1, rand2;
+    int i, j, temp, rand2;
     for (i = 0; i < 10; i++)
         answer[i] = i;  //正答用配列の初期化
     for (i = 0; i < 20; i++) {          //正答用配列の生成(20は任意の入れ替え回>数)
-        for (rand1 = 0; rand1 < 10; rand1++) {
-            rand2 = rand() % 10;
-            temp = answer[rand1];
-            answer[rand1] = answer[rand2];
-            answer[rand2] = temp;
+        for (j = 0; j < 10; j++) {
+            rand = rand() % 10;
+            temp = answer[j];
+            answer[j] = answer[rand];
+            answer[rand] = temp;
         }
     }
 }
